9f28825a123890ec53f479a6ec281caeec6d2626
Burhan Kundakcioglu Macbook <burhan.k@me.com>
2023-08-09 16:51:07 +0200

    First Initial Update

---
+++ README.md
@@ -0,0 +1,13 @@
+## Docker, Jenkins and Portainer Installation ##
+
+Run the following commands to install Docker, Jenkins and Portainer on your Ubuntu 18.04 server.
+
+```bash
+chmod +x start.sh && ./start.sh
+```
+
+
+This Script will install Docker and Docker Compose if not installed already. 
+After that it will start Jenkins and Portainer containers.
+
+Help me to improve this script by contributing to this repository.
---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+
+    # Install Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    echo "Docker has been installed."
+else
+    echo "Docker is already installed."
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is not installed. Installing Docker Compose..."
+
+    # Install Docker Compose
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+    echo "Docker Compose has been installed."
+else
+    echo "Docker Compose is already installed."
+fi
+
+# Run docker-compose.yml
+echo "Running docker-compose..."
+docker-compose -f docker-compose.yml up -d --build
