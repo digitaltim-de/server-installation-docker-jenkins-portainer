d5c11e9d23c43670c19c4352481b99e41412e696
Burhan Kundakcioglu Macbook <burhan.k@me.com>
2023-08-13 19:59:56 +0200

    Small fixes #2

--- start.sh
+++ start.sh
@@ -24,6 +24,7 @@ if ! command -v docker-compose &> /dev/null; then
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    sudo chmod 777 /var/run/docker.sock
 
     echo "Docker Compose has been installed."
 else
